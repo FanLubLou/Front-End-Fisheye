a73b865f82b4683917b43da9c488421fde3fa525
FanLubLou <jaclermont@hotmail.com>
2023-10-17 11:18:58 +0200

    Etape1-comprehension du code- j affiche ici les portraits et noms des photographes, je consolelog les autres donnée

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -3,23 +3,43 @@
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
         let photographers = [
             {
-                "name": "Ma data test",
+                "name": "EllieRoseWilkens",
                 "id": 1,
                 "city": "Paris",
                 "country": "France",
                 "tagline": "Ceci est ma data test",
                 "price": 400,
-                "portrait": "account.png"
+                "portrait": "EllieRoseWilkens.jpg"
             },
             {
-                "name": "Autre data test",
+                "name": "MarcelNikolic",
                 "id": 2,
                 "city": "Londres",
                 "country": "UK",
                 "tagline": "Ceci est ma data test 2",
                 "price": 500,
-                "portrait": "account.png"
+                "portrait": "MarcelNikolic.jpg"
             },
+
+            {
+                "name": "Mimi Keel",
+                "id": 3,
+                "city": "Londres",
+                "country": "UK",
+                "tagline": "Ceci est ma data test 2",
+                "price": 500,
+                "portrait": "MimiKeel.jpg"
+            },
+            {
+                "name": "Nabeel Bradford",
+                "id": 3,
+                "city": "Londres",
+                "country": "UK",
+                "tagline": "Ceci est ma data test 2",
+                "price": 500,
+                "portrait": "NabeelBradford.jpg"
+            },
+
         ]
         // et bien retourner le tableau photographers seulement une fois récupéré
         return ({
@@ -40,6 +60,10 @@
         // Récupère les datas des photographes
         const { photographers } = await getPhotographers();
         displayData(photographers);
+
+        console.log(photographers);
+        console.log(photographers[0])
+        console.log(photographers[0].name)
     }
     
     init();
