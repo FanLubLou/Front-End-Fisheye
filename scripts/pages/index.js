545dca40fc2b0b117453b7209af56ba16e9612c4
FanLubLou <jaclermont@hotmail.com>
2023-10-25 15:33:01 +0200

    ETape2 complete et quelques éléments d accessibilité ajoutés

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,49 +1,63 @@
     async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
+        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "EllieRoseWilkens",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "EllieRoseWilkens.jpg"
-            },
-            {
-                "name": "MarcelNikolic",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "MarcelNikolic.jpg"
-            },
+        
 
-            {
-                "name": "Mimi Keel",
-                "id": 3,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "MimiKeel.jpg"
-            },
-            {
-                "name": "Nabeel Bradford",
-                "id": 3,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "NabeelBradford.jpg"
-            },
+        // Etape 2 : Importer les datas
 
-        ]
+                // Je commente ici les données précédemment écrites à la main
+                        // let photographers = [
+                        //     {
+                        //         "name": "EllieRoseWilkens",
+                        //         "id": 1,
+                        //         "city": "Paris",
+                        //         "country": "France",
+                        //         "tagline": "Ceci est ma data test",
+                        //         "price": 400,
+                        //         "portrait": "EllieRoseWilkens.jpg"
+                        //     },
+                        //     {
+                        //         "name": "MarcelNikolic",
+                        //         "id": 2,
+                        //         "city": "Londres",
+                        //         "country": "UK",
+                        //         "tagline": "Ceci est ma data test 2",
+                        //         "price": 500,
+                        //         "portrait": "MarcelNikolic.jpg"
+                        //     },
+
+                        //     {
+                        //         "name": "Mimi Keel",
+                        //         "id": 3,
+                        //         "city": "Londres",
+                        //         "country": "UK",
+                        //         "tagline": "Ceci est ma data test 2",
+                        //         "price": 500,
+                        //         "portrait": "MimiKeel.jpg"
+                        //     },
+                        //     {
+                        //         "name": "Nabeel Bradford",
+                        //         "id": 3,
+                        //         "city": "Londres",
+                        //         "country": "UK",
+                        //         "tagline": "Ceci est ma data test 2",
+                        //         "price": 500,
+                        //         "portrait": "NabeelBradford.jpg"
+                        //     },
+
+                // ]
+        
+        // Etape 2 : Importer les datas // Récupération des données de la base de données
+        
+        const reponse = await fetch('./data/photographers.json');
+        const photographers = await reponse.json();
+        return photographers;
+
+        //Je commente l'ancienne méthode
         // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+    //     return ({
+    //         photographers: [...photographers, ...photographers, ...photographers]})
+     
     }
 
     async function displayData(photographers) {
@@ -64,6 +78,9 @@
         console.log(photographers);
         console.log(photographers[0])
         console.log(photographers[0].name)
+        
+        console.log(global)
+
     }
     
     init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,16 +1,55 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    // Etape 2, j'enrichis la fonction pour pouvoir récupérer toutes les données
+    const { name, id, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+
+        // Etape#2: 
+        // Je créé ici un container pour l image pour gérer le cercle
+        const img_container = document.createElement('div');
+        img_container.className="image-container"
+
+        const h2 = document.createElement('h2');
         h2.textContent = name;
-        article.appendChild(img);
+
+        const h3 = document.createElement('h3');
+        h3.textContent = city + ", " + country;
+
+        const h4 = document.createElement('h4');
+        h4.textContent = tagline;
+
+        const id_p = document.createElement('p');
+        id_p.textContent = id;
+
+        
+
+        const price_p = document.createElement('p');
+        price_p.textContent = price + "€/jour";
+        
+        
+
+        // Etape#2:
+        // img_container est enfant de l article
+        article.appendChild(img_container);
+        // Etape#2:
+        // img est enfant de img_container
+        img_container.appendChild(img);
+
+        // article.appendChild(id_p);
+        // article.appendChild(tagline_p);
+        // article.appendChild(price_p);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(h4);
+        article.appendChild(price_p);
+        
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
